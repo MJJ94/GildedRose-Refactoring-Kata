a1d0d47b756938b0a48ff9adea5da7109f3ac6ae
MJ <m.jammal94@gmail.com>
2023-05-01 18:44:33 +0200

    Conjured items handling #5 Added implementation based on the requirements

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -5,6 +5,7 @@ import com.gildedrose.exceptions.ItemsNullOrEmptyExceptions;
 import com.gildedrose.handlers.ItemHandler;
 import com.gildedrose.handlers.agedbrie.AgedBrieHandler;
 import com.gildedrose.handlers.backstagepass.BackstagePassHandler;
+import com.gildedrose.handlers.conjured.ConjuredItemHandler;
 import com.gildedrose.handlers.normal.NormalItemHandler;
 import com.gildedrose.handlers.sulfuras.SulfurasHandler;
 
@@ -23,10 +24,12 @@ class GildedRose {
         ItemHandler backstagePassHandler = new BackstagePassHandler();
         ItemHandler agedBrieHandler = new AgedBrieHandler();
         ItemHandler sulfurasHandler = new SulfurasHandler();
+        ItemHandler conjuredHandler = new ConjuredItemHandler();
 
         sulfurasHandler.setNext(agedBrieHandler);
         agedBrieHandler.setNext(backstagePassHandler);
-        backstagePassHandler.setNext(normalItemHandler);
+        backstagePassHandler.setNext(conjuredHandler);
+        conjuredHandler.setNext(normalItemHandler);
 
         for (Item item : items) {
             if (Objects.isNull(item.name) || item.name.isEmpty()) {
